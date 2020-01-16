b1c6804187eeb7567dd1408d69973ad745a5a090
MarikTar <100trk@gmail.com>
2020-01-16 15:07:47 +0200

    add some correct

--- src/parser/OlxParser/index.js
+++ src/parser/OlxParser/index.js
@@ -1,14 +1,14 @@
 import HTMLParser from '../HTMLParser';
 
 export default class OlxParser extends HTMLParser {
-  async getPagesCount(param) {
-    const arr = await this.parseHTML(param);
+  async getPagesCount() {
+    const arr = await this.parseHTML('a.block.br3.brc8.large.tdnone.lheight24 span');
     // console.log(+arr[arr.length - 1].childNodes[0].rawText);
     return +arr[arr.length - 1].childNodes[0].rawText;
   }
 
-  async extractData(param) {
-    // const lng = await this.getPagesCount(param);
+  async extractData() {
+    // const lng = await this.getPagesCount();
     const itemsArr = [];
     for (let i = 1; i <= 2; i += 1) {
       const data = this.parseHTML(`${this.URL}?page=${i}`);
