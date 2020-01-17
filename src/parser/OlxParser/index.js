73a2a3c03f98f46bb4dcbbce45161fbe31846e55
MarikTar <100trk@gmail.com>
2020-01-17 15:41:30 +0200

    first worked example

--- src/parser/OlxParser/index.js
+++ src/parser/OlxParser/index.js
@@ -9,14 +9,14 @@ export default class OlxParser extends HTMLParser {
 
   async parseMainPages(param) {
     const lng = await this.getPagesCount();
-    const items = [];
-    for (let i = 1; i <= lng; i += 1) {
+    const itemPromises = [];
+    for (let i = 1; i <= 1; i += 1) {
       const data = this.parseHTML(param, `${this.URL}?page=${i}`);
-      items.push(data);
+      itemPromises.push(data);
     }
-    // await Promise.all(items);
-    const fetchedItems = await Promise.all(items);
-    return fetchedItems.flat();
+    // await Promise.all(itemPromises);
+    const res = await Promise.all(itemPromises);
+    return res.flat();
   }
 
   async extractData(param) {
@@ -30,11 +30,30 @@ export default class OlxParser extends HTMLParser {
     });
   }
 
-  async as(param) {
+  async parseView(param) {
     await this.extractData(param);
+    const viewPromises = [];
     this.elmArr.forEach((elm) => {
-      const data = this.parseHTML('#offerbottombar.pdingtop10 strong', elm.href);
-      console.log(data);
+      const data = this.parseHTML('.pdingtop10 strong', elm.href);
+      viewPromises.push(data);
     });
+    const res = await Promise.all(viewPromises);
+    const views = res.flat();
+
+    this.elmArr.forEach((elma, i) => {
+      this.elmArr[i].view = +views[i].childNodes[0].rawText;
+    });
+  }
+
+  async sortLargerToSmaller(param) {
+    await this.parseView(param);
+    this.elmArr.sort((a, b) => b.view - a.view);
+    console.log(this.elmArr);
+  }
+
+  async sortSmallerToLarger(param) {
+    await this.parseView(param);
+    this.elmArr.sort((a, b) => a.view - b.view);
+    console.log(this.elmArr);
   }
 }
